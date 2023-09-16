37905cb0c7f071b0f8a7cb15290a054df9c1fd61
EdwardGold Edem Hogan <edge37@outlook.com>
2023-09-16 21:12:45 +0000

    .

--- src/lib/util/store.ts
+++ src/lib/util/store.ts
@@ -1,9 +1,9 @@
-import { browser } from '$app/environment';
+import { BROWSER } from 'esm-env';
 import { writable, type Writable } from 'svelte/store';
 
 export const booleanStore = (key = '', initialValue = false) => {
 	let previousValue: boolean;
-	if (browser) {
+	if (BROWSER) {
 		const fromLocalStorage = localStorage.getItem(key);
 		if (fromLocalStorage) {
 			previousValue = JSON.parse(fromLocalStorage);
@@ -14,13 +14,13 @@ export const booleanStore = (key = '', initialValue = false) => {
 		previousValue = initialValue;
 	}
 	const s = writable<boolean>(previousValue);
-	if (browser) s.subscribe((v) => localStorage.setItem(key, String(v)));
+	if (BROWSER) s.subscribe((v) => localStorage.setItem(key, String(v)));
 	return s;
 };
 
 export const numberStore = (key = '', initialValue = 0): Writable<number> => {
 	let previousValue: number;
-	if (browser) {
+	if (BROWSER) {
 		const fromLocalStorage = localStorage.getItem(key);
 		if (fromLocalStorage) {
 			previousValue = Number(fromLocalStorage);
@@ -31,25 +31,25 @@ export const numberStore = (key = '', initialValue = 0): Writable<number> => {
 		previousValue = initialValue;
 	}
 	const s = writable<number>(previousValue);
-	if (browser) s.subscribe((v) => localStorage.setItem(key, String(v)));
+	if (BROWSER) s.subscribe((v) => localStorage.setItem(key, String(v)));
 	return s;
 };
 
 export const stringStore = (key = '', initialValue = '') => {
 	let previousValue: string;
-	if (browser) {
+	if (BROWSER) {
 		previousValue = localStorage.getItem(key) || initialValue;
 	} else {
 		previousValue = initialValue;
 	}
 	const s = writable<string>(previousValue);
-	if (browser) s.subscribe((v) => localStorage.setItem(key, v));
+	if (BROWSER) s.subscribe((v) => localStorage.setItem(key, v));
 	return s;
 };
 
 export const arrayStore = <Type>(key = '', initialValue: Array<Type>) => {
 	let previousValue: Array<Type>;
-	if (browser) {
+	if (BROWSER) {
 		const fromLocalStorage = localStorage.getItem(key);
 		if (fromLocalStorage) {
 			previousValue = JSON.parse(fromLocalStorage);
@@ -60,13 +60,13 @@ export const arrayStore = <Type>(key = '', initialValue: Array<Type>) => {
 		previousValue = initialValue;
 	}
 	const s = writable<Array<Type>>(previousValue);
-	if (browser) s.subscribe((v) => localStorage.setItem(key, JSON.stringify(v)));
+	if (BROWSER) s.subscribe((v) => localStorage.setItem(key, JSON.stringify(v)));
 	return s;
 };
 
 export const objectStore = (key = '', initialValue: object) => {
 	let previousValue: object;
-	if (browser) {
+	if (BROWSER) {
 		const fromLocalStorage = localStorage.getItem(key);
 		if (fromLocalStorage) {
 			previousValue = JSON.parse(fromLocalStorage);
@@ -77,13 +77,13 @@ export const objectStore = (key = '', initialValue: object) => {
 		previousValue = initialValue;
 	}
 	const s = writable<object>(previousValue);
-	if (browser) s.subscribe((v) => localStorage.setItem(key, JSON.stringify(v)));
+	if (BROWSER) s.subscribe((v) => localStorage.setItem(key, JSON.stringify(v)));
 	return s;
 };
 
 export const typeStore = <Type>(key = '', initialValue: Type, resolve: (v: string) => Type) => {
 	let previousValue: Type;
-	if (browser) {
+	if (BROWSER) {
 		const fromLocalStorage = localStorage.getItem(key);
 		if (fromLocalStorage) {
 			previousValue = resolve(fromLocalStorage)
@@ -94,7 +94,7 @@ export const typeStore = <Type>(key = '', initialValue: Type, resolve: (v: strin
 		previousValue = initialValue;
 	}
 	const s = writable<Type>(previousValue);
-	if (browser)
+	if (BROWSER)
 		s.subscribe((v) => {
 			localStorage.setItem(key, JSON.stringify(v));
 		});
